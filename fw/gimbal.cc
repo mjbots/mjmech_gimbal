542859f2998dd0ccd76efe0806ef8a28234f2a81
Josh Pieper <jjp@pobox.com>
2019-04-27 21:07:50 -0400

    BLDC outputs now work

--- fw/gimbal.cc
+++ fw/gimbal.cc
@@ -263,6 +263,35 @@ void InitTimers(TIM_HandleTypeDef* htim2,
                 TIM_HandleTypeDef* htim3,
                 TIM_HandleTypeDef* htim4,
                 TIM_HandleTypeDef* htim5) {
+  {
+    GPIO_InitTypeDef GPIO_InitStruct;
+
+    __TIM2_CLK_ENABLE();
+
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    __TIM3_CLK_ENABLE();
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_4|GPIO_PIN_5;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    __TIM4_CLK_ENABLE();
+    GPIO_InitStruct.Pin = GPIO_PIN_8;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF2_TIM4;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  }
+
   // This is copied from CubeMX auto-generated crud.
   {
     TIM_ClockConfigTypeDef sClockSourceConfig;
@@ -351,6 +380,7 @@ void InitTimers(TIM_HandleTypeDef* htim2,
     sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
     HAL_TIM_PWM_ConfigChannel(htim4, &sConfigOC, TIM_CHANNEL_3);
   }
+
 }
 }
 
